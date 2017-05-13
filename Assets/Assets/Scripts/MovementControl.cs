9e856ec9a3762f0d566aeb7849f76410655afffa
mihane <mikiko131@icloud.com>
2017-05-13 16:09:11 +0900

    コミット

---
+++ Assets/Assets/Scripts/MovementControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovementControl : MonoBehaviour {
+	public WheelCollider[] wheelColliders = new WheelCollider[4];
+	public Transform[] tyreMeshes = new Transform[4];
+	public float maxTorque = 50.0f;
+	private Rigidbody m_rigidbody;
+	public Transform centerOfMass;
+
+	void start()
+	{
+		m_rigidbody = GetComponent<Rigidbody>();
+		m_rigidbody.centerOfMass = centerOfMass.localPosition;
+	}
+	
+	void Update()
+	{
+		UpdateMeshesPositions ();
+	}
+	
+	void FixedUpdate()
+	{// 0 is front left and 1 is front right
+		float steer = Input.GetAxis ("Horizontal");
+		float fixedAngel = steer * 45f;
+		wheelColliders [0].steerAngle = fixedAngel;
+		wheelColliders [1].steerAngle = fixedAngel;
+
+		////
+		float acceleration = Input.GetAxis ("Vertical");
+		for (int i = 0; i < 4; i++) 
+		{
+			Invoke ("DelayMethod", 30.0f);
+			void DelayMethod (){
+				wheelColliders[i].motorTorque = 1f * maxTorque;
+			}
+		}
+
+		////
+	}
+	
+	void UpdateMeshesPositions()
+	{
+		for(int i = 0; i < 4 ; i++)
+		{
+			Quaternion quat;
+			Vector3 pos;
+			wheelColliders[i].GetWorldPose(out pos, out quat);
+			tyreMeshes[i].position = pos;
+			tyreMeshes[i].rotation = quat;
+		}
+	}
+}
---
+++ Assets/Touch.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Touch : MonoBehaviour {
+	// rayが届く範囲
+	public float distance = 100f;
+
+	public GameObject[] tsumini = new GameObject[5];
+
+	public GameObject cube;
+
+	private Vector3 clickPosition;
+
+	public int hantei;
+
+	void Update () {
+		// 左クリックを取得
+		if (Input.GetMouseButtonDown(0)) {
+			// クリックしたスクリーン座標をrayに変換
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			// Rayの当たったオブジェクトの情報を格納する
+			RaycastHit hit = new RaycastHit();
+			// オブジェクトにrayが当たった時
+			if (Physics.Raycast(ray, out hit, distance)) {
+				// rayが当たったオブジェクトの名前を取得
+				string objectName = hit.collider.gameObject.name;
+				Debug.Log(objectName);
+
+				clickPosition = Input.mousePosition;
+
+				clickPosition.z = 5f;
+
+				Instantiate (tsumini[hantei], Camera.main.ScreenToWorldPoint (clickPosition), cube.transform.rotation);
+			}
+		}
+	}
+
+	public void Box (){
+		hantei = 0;
+	}
+
+	public void ball (){
+		hantei = 1;
+	}
+
+	public void TV (){
+		hantei = 2;
+	}
+}
